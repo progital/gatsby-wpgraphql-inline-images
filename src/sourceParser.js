28f24fefc673e4018811bdab5e4560f27623bd72
robmarshall <robertmarshllproductions@gmail.com>
2019-10-05 12:44:41 +0100

    Updated entities

--- src/sourceParser.js
+++ src/sourceParser.js
@@ -70,7 +70,7 @@ module.exports = async function sourceParser(
     return `${pathPrefix}/static/${fileName}`;
   };
 
-  const $ = cheerio.load(content, { xmlMode: true });
+  const $ = cheerio.load(content, { xmlMode: true, decodeEntities: false });
 
   let imageRefs = [];
   let pRefs = [];
@@ -204,7 +204,7 @@ module.exports = async function sourceParser(
     }
 
     $(item).attr('src', swapVal.src);
-    $(item).attr('data-gts-encfluid', swapVal.encoded);
+    $(item).attr('data-gts-encfluid', swapVal.encoded.replace(/"/g, '&quot;'));
     $(item).removeAttr('srcset');
     $(item).removeAttr('sizes');
   });
