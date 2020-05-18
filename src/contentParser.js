0c47854bc1656d41b2cd9197040410bc329e953c
Johnny McKinney <johnnym@rollingpaperdepot.com>
2020-05-18 18:04:14 -0400

    Added handling for anchors

--- src/contentParser.js
+++ src/contentParser.js
@@ -37,6 +37,15 @@ export default function contentParser(
     const subdir = wordPressUrlParsed.path();
     return path.replace(subdir, '/');
   };
+	
+	const getAnchor = (url) => {
+		let anchor = '#' + url.split('#')[1];
+		if (!url.includes('#')) {
+			return;
+		} else {
+			return anchor;
+		}
+	};
 
   const parserOptions = {
     replace: domNode => {
@@ -48,7 +57,8 @@ export default function contentParser(
       if (!elementUrl) {
         return;
       }
-
+			
+			let urlAnchor = getAnchor(elementUrl);
       let urlParsed = new URIParser(elementUrl);
 
       // TODO test if this hash handling is sufficient
@@ -86,11 +96,19 @@ export default function contentParser(
       ) {
         let url = urlParsed.path();
         url = subdirectoryCorrection(url, wordPressUrl);
-        return (
-          <Styled.a as={Link} to={url} className={className}>
-            {domToReact(domNode.children, parserOptions)}
-          </Styled.a>
-        );
+				if (urlAnchor) {
+					return (
+						<Styled.a as={Link} to={url + urlAnchor} className={className}>
+							{domToReact(domNode.children, parserOptions)}
+						</Styled.a>
+					);
+				} else {
+					return (
+						<Styled.a as={Link} to={url} className={className}>
+							{domToReact(domNode.children, parserOptions)}
+						</Styled.a>
+					);
+				}
       }
 
       // cleans up internal processing attribute
